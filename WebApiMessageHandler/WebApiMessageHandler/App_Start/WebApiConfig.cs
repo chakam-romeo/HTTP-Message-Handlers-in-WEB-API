d755bc303402987ed443fbf8f8254cdea4227bc9
romeo <romeo.chakam@cash-xpress.ch>
2019-09-07 10:46:48 +0100

    Exemple d'api de gestion des messages d'autorisation

---
+++ WebApiMessageHandler/WebApiMessageHandler/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApiMessageHandler.Models;
+
+namespace WebApiMessageHandler
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuration et services API Web
+            config.MessageHandlers.Add(new MessageHandler1());
+            config.MessageHandlers.Add(new MessageHandler2());
+            // Itinéraires de l'API Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApiMessageHandler/WebApiMessageHandler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiMessageHandler.Global" Language="C#" %>
